bf9426ec4e99bb3c8b8fc21a147d0f931ef657ed
mouylang <mouylang.flechard@gmail.com>
2021-01-02 21:59:08 +0100

    formulaire  room debut

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Room;
 use App\Repository\RoomRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RoomController extends AbstractController
 {
@@ -22,6 +24,35 @@ class RoomController extends AbstractController
             'rooms' => $rooms
         ]);
     }
+
+    /**
+     * @Route("/room/new", name="room_create")
+     */
+    public function create(){
+        $room = new Room();
+
+        $formBuilder = $this->createFormBuilder($room);
+        $formBuilder->add('title', TextType::class, [
+            'attr'=>[
+                'placeholder'=> 'saisir un titre'
+            ]
+        ]);
+        $formBuilder->add('country');
+        $formBuilder->add('city');
+        $formBuilder->add('address');
+        $formBuilder->add('zipCode');
+        $formBuilder->add('description');
+        $formBuilder->add('photo');
+        $formBuilder->add('capacity');
+        $formBuilder->add('category');
+        $form=$formBuilder->getForm();
+        $formView = $form->createView();
+ 
+        return $this->render('room/create.html.twig',[
+            'formRoom' =>  $formView
+        ]);
+
+    }
     
     /**
      * @Route("/room/{id}", name="room_show")
@@ -32,6 +63,9 @@ class RoomController extends AbstractController
             'room' => $room
         ]);
     }
+
+    
+    
 
 
 }
